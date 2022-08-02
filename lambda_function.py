8273677f52447c3ced1de19dd0d198d9a6b5b140
SEONGHOON <89100999+Dan-hoon@users.noreply.github.com>
2022-08-02 15:36:56 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,6 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
 
-        'body': json.dumps('Hello from Lambda!!!!!!!!!!@@@$%^^332')
+        'body': json.dumps('Hello from Lambda!!!!!!!!!!@@@$%^^336')
 
     }
